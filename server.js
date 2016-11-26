6a079ad26d7d12b71846ce34a78e45cc46b7c93c
DamianStanger <damian.stanger+github@gmail.com>
2016-11-26 20:58:26 +0000

    added in memory cards

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 
 const
     Hapi = require('hapi'),
-    server = new Hapi.Server();
+    server = new Hapi.Server(),
+    uuid = require('uuid'),
+    cards = {};
 
 server.connection({port:3000});
 
@@ -38,16 +40,37 @@ server.route({
 });
 
 server.route({
+    path: '/cards/{id}',
+    method: 'DELETE',
+    handler: deleteCardHandler
+});
+
+server.route({
     path: '/cards',
     method: 'GET',
     handler: cardsHandler
 });
 
+function saveCard(card) {
+        var id = uuid.v1();
+        card.id = id;
+        cards[id] = card;
+}
 
 function newCardHandler(request, reply) {
     if(request.method==='get') {
         reply.file('templates/new.html');
     } else {
+        var card = {
+            name: request.payload.name,
+            recipient_email: request.payload.recipient_email,
+            sender_name: request.payload.sender_name,
+            sender_email: request.payload.sender_email,
+            card_image: request.payload.card_image
+        }
+        saveCard(card);
+        console.log(cards);
+
         reply.redirect('/cards')
     }
 };
@@ -55,6 +78,9 @@ function cardsHandler (request, reply) {
     reply.file('templates/cards.html');
 };
 
+function deleteCardHandler(request, reply){
+    delete cards[request.params.id];
+}
 
 server .start(function(){
     console.log('Listening on ' + server.info.uri);
